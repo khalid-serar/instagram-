34c11ae64c4f200d3b5d942c9f371184abf964a7
khalid-serar <khalid.serar@student.moringaschool.com>
2021-12-06 12:59:53 +0300

    modify settings

--- instagram/settings.py
+++ instagram/settings.py
@@ -16,7 +16,9 @@ import django_heroku
 import cloudinary
 import cloudinary.uploader
 import cloudinary.api
-
+import django_heroku
+import dj_database_url
+from decouple import config,Csv
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -164,3 +166,5 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 # Configure Django App for Heroku.
 django_heroku.settings(locals())
+
+
